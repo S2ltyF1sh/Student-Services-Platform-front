6b0605c5bc79d5aa2fadaa73f0805f0d194310d0
S2ltyF1sh <rojasgraves82@gmail.com>
2025-09-16 00:00:24 +0800

    feat(login,main) 添加了登录页面和主页面; test(login) 登录页面基本框架

---
+++ SSP/src/login/loginMain.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- 登录界面 -->
+  <div class="loginMain">
+
+    <div class="Container">
+      <header>
+        <h1>欢迎使用SSP</h1>
+      </header>
+      <!-- 登录详细块 -->
+      <div class="loginContainer" v-if="loginState === 'login'">
+        登录详细块
+      </div>
+      <!-- 注册详细块 -->
+      <div class="registContainer" v-else>
+        注册详细块
+      </div>
+      <!-- 功能按钮 -->
+      <div class="functionButton">
+        <button class="bigButton" @click="toggleState()">{{ toggleButtonContext }}</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const loginState = ref('login')
+
+const toggleButtonContext = computed(() => {
+  switch(loginState.value){
+    case 'login': return '切换为注册'
+    case 'regist': return '切换为登录'
+    default: return '切换'
+  }
+})
+
+const toggleState = () => {loginState.value = loginState.value === 'login' ? 'regist' : 'login'}
+
+</script>
+
+<style scoped>
+@import url('./loginMain.css');
+@import url('../style/appliedButton.css');
+@import url('../style/input.css');
+</style>
--- SSP/src/router/index.ts
+++ SSP/src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '@/main/mainView.vue'
+import loginMain from '@/login/loginMain.vue'
 
 const routes =[
   {
     path:'/',
     name:'main',
     component:MainView
-  }
+  },
+  {
+    path:'/login',
+    name:'login',
+    component:loginMain
+  },
 ]
 
 const router = createRouter({
